d1b3a363cc17bc913b0e534bf3aa79af9ecf5efc
shenbingyan <shenby@server03>
2019-05-27 18:50:45 +0800

    add lrc file to Test

--- app/src/main/java/com/by5388/mymediaplayer/other/Lyric.java
+++ app/src/main/java/com/by5388/mymediaplayer/other/Lyric.java
@@ -1,5 +1,7 @@
 package com.by5388.mymediaplayer.other;
 
+import android.util.Log;
+
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -9,6 +11,8 @@ import java.util.List;
  * @author Administrator  on 2019/5/24.
  */
 public class Lyric implements Serializable {
+    // TODO: 2019/5/27 check fileName's suffix
+    public static final String LYRIC_SUFFIX = ".lrc";
     /**
      * 行数
      */
@@ -21,7 +25,12 @@ public class Lyric implements Serializable {
     List<LyricText> mLyricTexts;
 
     Lyric(File file) {
+        if (file == null || file.isDirectory() || file.exists() || !file.getName().toLowerCase().endsWith(LYRIC_SUFFIX)) {
+            Log.e(TAG, "Lyric: ", new IllegalArgumentException());
+            this();
+        } else {
 
+        }
     }
 
     Lyric() {
